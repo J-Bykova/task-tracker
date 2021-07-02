1b2ec74b63d29943465fdf601474925f9dac54b4
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-07-01 17:34:48 -0700

    Add deleteTask function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
@@ -26,10 +26,17 @@ export default function App() {
     ]
   );
 
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id != id))
+  }
+
   return (
     <div className="container">
-      <Header />
-      <Tasks tasks={tasks}/>
+      <Header/>
+      <Tasks
+        tasks={tasks}
+        onDelete={deleteTask}
+      />
     </div>
   );
 }
