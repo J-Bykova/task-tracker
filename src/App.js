249a3ccbd10c5b43859220894bcf8da649671425
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-07-22 14:04:47 -0700

    Delete task from server

--- src/App.js
+++ src/App.js
@@ -28,7 +28,10 @@ export default function App() {
     setTasks([...tasks, newTask]);
   }
 
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
     setTasks(tasks.filter((task) => task.id !== id));
   }
 
